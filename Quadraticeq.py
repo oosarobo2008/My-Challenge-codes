df3961eea2087243e58a86093c0e7a4823aa3cad
oosarobo2008 <oosarobo2008@yahoo.com>
2022-08-26 15:40:23 +0100

    added file on Quadratic

---
+++ Quadraticeq.py
@@ -0,0 +1,17 @@
+# Question solve the quadratic equation ax**2 + bx + c = 0 for given vslue of a,b and c
+
+# import complex math module
+import cmath
+
+a = int(input('Enter Value for a :'))
+b = int(input('Enter Value for b:'))
+c = int(input('Enter value for c:'))
+
+# calculate the discriminant
+d = (b**2) - (4*a*c)
+
+# find two solutions
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
+
+print('The solution are {0} and {1}'.format(sol1,sol2))
